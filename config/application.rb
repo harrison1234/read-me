36fbaab03f1f496cdc434564439796cb6aaec86b
Harrison Page <harrison.g.page@gmail.com>
2016-09-08 00:08:03 +1000

    implemented devise

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Vews
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+#needed for heroku
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
