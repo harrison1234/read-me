954ec8f6689135ab979d040ebf16da5ad6537aa7
Harrison Page <harrison.g.page@gmail.com>
2016-09-09 11:34:37 +1000

    add pins/usersassociation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,11 @@ validates :password, :presence => true,
                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*\Z/ }
 
 
-
-
-
-
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         has_many :pins
 end
 
